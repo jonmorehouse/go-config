fbbcd271e2bc8545ec6340734d3f20720cf06cc7
Jon Morehouse <morehousej09@gmail.com>
2014-02-11 16:49:39 -0800

    Temporary commit. Continue getting more comfortbale with the structure of the package

--- config/config.go
+++ config/config.go
@@ -2,32 +2,75 @@ package config
 
 import (
 
-
+	"errors"
+	"os"
 )
 
 type Config struct {
 
+	// map all configuration elements to the variables as needed
 	elements map[string]interface{}
-
 }
 
-func Set(key string, defaultValue interface{}) {
+// this is the variable that will hold all of our elements as needed 
+var internal Config 
 
-	// initialize set methods as needed
-	switch defaultValue.(type) {
+func bootstrap() {
 
-	case int:
-		fmt.Println()
-	case nil:
-		fmt.Println()
+	// bootstrap our configuration environment
+	if internal == (Config{elements:}) {
 
+		//internal = new(config) 
+		internal.elements = make(map[string]interface{})
 	}
+}
+
+func Set(key string, defaultValue interface{}) error {
+
+	// lets check to make sure that we have a valid internal element?
+		
+	// initialize return variable
+	var returnError error
+
+	// grab the current element
+	element := internal.elements[key]
+
+	// now lets 
+	if element == nil {
+
+		// now lets get the environment variable
+		value := os.Getenv(key)
+
+		if value == "" && defaultValue == nil {
+
+			returnError = errors.New("Environment variable not found. No default passed")
+
+		} else if value == "" { // no variable found -- use default
+
+			// set the environment variable
+			internal.elements[key] = defaultValue
+
+			// now create an error to be returned
+			returnError = errors.New("Environment variable not found. Using default value")
+
+		} else { // we have a variable here to use!
+
+			internal.elements[key] = value
+		}
+
+	} else {
+
+		// this variable already exists - print something?
+		returnError = errors.New("Key exists")
+	}
+
+	// we passed - no need to return
+	return returnError
 }
 
 func Get(key string) (interface{}, error) {
 
 	// get the element as needed
-	
 	return 55, nil
 }
 
--- config/config_test.go
+++ config/config_test.go
@@ -3,11 +3,28 @@ package config
 import (
 
 	"testing"
+	"fmt"
 )
 
-func TestConfigAttributes(t *testing.T) {
+func TestSet(t *testing.T) {
 
 	// now lets test the config attribute
+	result := Set("HOST", "go-test-host")
+	
+	if result != nil {
+
+		fmt.Println("FIAL")
+
+	} else {
+
+
+		fmt.Println(internal.elements["HOST"])
+
+	}
+	
+
+
+
 
 }
 
